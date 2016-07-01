bce024fec5eadbab6f8ce51b2e9e98e88343355e
LEThomas <zaev@promnov.ru>
2016-07-01 10:54:44 +0300

    Revert "revert to redis acl after fix to ENV"
    This reverts commit 8bd71a193d006635eb7314e6bfcd6e3f7c80e3c2.

--- modules/users/server/policies/admin.server.policy.js
+++ modules/users/server/policies/admin.server.policy.js
@@ -3,24 +3,26 @@
 var
   path = require('path'),
   config = require(path.resolve('./config/config')),
-  redisInstance = require('redis').createClient(process.env.REDIS_URL),
+  //redisInstance = require('redis').createClient(process.env.REDIS_URL),
   acl = require('acl');
 
 /**
  * Module dependencies.
  */
 
+/*
 // Using the redis backend
 
-//Use redis database 0
-redisInstance.select(0);
+//Use redis database 1
+redisInstance.select(1);
 
 if (config.redis.password) {
   redisInstance.auth(config.redis.password);
 }
 
 acl = new acl(new acl.redisBackend(redisInstance, 'acl'));
-//acl = new acl(new acl.memoryBackend());
+*/
+acl = new acl(new acl.memoryBackend());
 
 /**
  * Invoke Admin Permissions
