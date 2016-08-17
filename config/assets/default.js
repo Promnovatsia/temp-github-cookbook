46a7c5721656cdd16931dc19bbc9756c5e12c99d
LEThomas <zaev@promnov.ru>
2016-08-17 13:25:56 +0300

    локализация

--- config/assets/default.js
+++ config/assets/default.js
@@ -9,6 +9,7 @@ module.exports = {
       ],
       js: [
         'public/lib/angular/angular.js',
+        'public/lib/bower-angular-i18n-master/angular-locale_ru-ru.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
         'public/lib/angular-messages/angular-messages.js',
