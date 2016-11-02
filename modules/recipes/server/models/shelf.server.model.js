9dae742787fd96eec0e2c28951631068e9724ca4
LEThomas <zaev@promnov.ru>
2016-11-02 16:44:30 +0300

    feat: изменение модели shelf add: дополнительные флаги add: scope для shelf

--- modules/recipes/server/models/shelf.server.model.js
+++ modules/recipes/server/models/shelf.server.model.js
@@ -3,40 +3,29 @@
 module.exports = function (sequelize, DataTypes) {
 
     var Shelf = sequelize.define('shelf', {
-        
-        number: {
-            type: DataTypes.INTEGER    
-        },
-        caption: {
-            type: DataTypes.STRING        
-        },
-        measureCaption: {
-            type: DataTypes.STRING        
-        },
-        stored: {
-            type: DataTypes.FLOAT
-        },
-        desired: {
-            type: DataTypes.FLOAT
-        },
-        max: {
-            type: DataTypes.FLOAT
-        },
-        deficit: {
-            type: DataTypes.FLOAT
-        },
-        isSpoiled: {
+
+        isClosed: {
             type: DataTypes.BOOLEAN,
             allowNull: false,
             defaultValue: false
         },
-        place: {
-            type: DataTypes.STRING
-        },
-        lastQuery: {
-            type: DataTypes.INTEGER,
+        isRevision: {
+            type: DataTypes.BOOLEAN,
             allowNull: false,
-            defaultValue: 0
+            defaultValue: false
+        },
+        isStorage: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        isResource: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        number: {
+            type: DataTypes.INTEGER    
         },
         override: {
             type: DataTypes.INTEGER,
@@ -51,16 +40,59 @@ module.exports = function (sequelize, DataTypes) {
                 model: "shelves",
                 key: "id"
             }
+        },
+        stored: {
+            type: DataTypes.FLOAT
+        },
+        deficit: {
+            type: DataTypes.FLOAT
+        },
+        desired: {
+            type: DataTypes.FLOAT
+        },
+        max: {
+            type: DataTypes.FLOAT
+        },
+        caption: {
+            type: DataTypes.STRING        
+        },
+        place: {
+            type: DataTypes.STRING
+        },
+        measureCaption: { //TODO remove as premature optimization
+            type: DataTypes.STRING
         }
-        /* FUTURE product integration
-        product: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "products",
-                key: "id"
-            }
-        },*/
     }, {
+        scopes: {
+            open: {
+                where: {
+                    isDeleted: false,
+                    isClosed: false,
+                    isStorage: false,
+                    isRevision: false
+                }
+            },
+            restock: {
+                where: {
+                    isDeleted: false,
+                    isClosed: false,
+                    isStorage: false,
+                    $or: [
+                        {
+                            stored: {
+                                $lt: sequelize.col('deficit')
+                            }
+                        },
+                        {
+                            isResource: true,
+                            stored: {
+                                $lt: sequelize.col('desired')
+                            }
+                        }
+                    ]
+                }
+            }
+        },
         timestamps: true,
         createdAt: false,
         paranoid: true,
@@ -68,7 +100,7 @@ module.exports = function (sequelize, DataTypes) {
             Shelf.belongsTo(models.user);
             Shelf.belongsTo(models.ingredient);
             Shelf.belongsTo(models.measure);
-            Shelf.hasMany(models.shelfQuery);
+            Shelf.hasMany(models.request);
         }
     });
     
